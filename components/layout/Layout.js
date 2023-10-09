3cd6f5818a3bc77943b1cb4732e5811188cf6f31
NEX-DoT <nex.dot2@gmail.com>
2023-10-09 18:43:31 +0330

    part 1 is done

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 const Layout = ({children}) => {
     return (
         <div>
-           <header className="flex  w-full items-center justify-around h-20">
+           <header className="flex w-full items-center justify-around h-20">
                <h2 className="text-xl">Botostart CRM</h2>
                <Link className="bg-green-500 p-2 rounded-md" href="/add-customer">Add Customer</Link>
            </header>
---
+++ utils/connectDB.js
@@ -0,0 +1,10 @@
+import {mongoose , mongo} from "mongoose";
+
+
+async function connectDB  () {
+    if(mongoose.connection[0]) return;
+    await mongoose.connect(process.env,BASE_URL) ;
+    console.log("Connected to DB");
+};
+
+export default connectDB;
