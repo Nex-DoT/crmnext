27c2195288c78d4beda58d8d8719f5fc7f3bfd0d
NEX-DoT <nex.dot2@gmail.com>
2023-10-09 17:11:27 +0330

    Layout added

---
+++ components/layout/Layout.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+const Layout = ({children}) => {
+    return (
+        <div>
+           <header className="flex  w-full items-center justify-around h-20">
+               <h2 className="text-xl">Botostart CRM</h2>
+               <Link className="bg-green-500 p-2 rounded-md" href="/add-customer">Add Customer</Link>
+           </header>
+           <main className="w-full h-fullScreen min-h-screen">{children}</main>
+           <footer className="flex  justify-center">
+               Next.js course | CRM Project &copy;  
+           </footer>            
+        </div>
+    );
+};
+export default Layout;
