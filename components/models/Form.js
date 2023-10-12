935f843af7c2c6f65bdd9f0e6903e9428db355c6
NEX-DoT <nex.dot2@gmail.com>
2023-10-12 17:57:08 +0330

    on change form state

--- components/models/Form.js
+++ components/models/Form.js
@@ -1,31 +1,37 @@
 const Form = ({form , setForm}) => {
     console.log(form);
+    const onChangeHandeler = (e) => {
+        setForm({
+            ...form,
+            [e.target.name] : e.target.value,
+        })
+    };
     return (
         <form className="w-full">
             <div className="w-full p-5">
                 <div>
                     <h2 className="text-xl text-white m-2">Name</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="name"value={form.name}  onChange={onChangeHandeler} />
                 </div>
                 <div>
                     <h2 className="text-xl text-white m-2">Last Name</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="lastName"value={form.lastName}  onChange={onChangeHandeler} />
                 </div>
                 <div>
                     <h2 className="text-xl text-white m-2">Email</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="email"value={form.email}  onChange={onChangeHandeler} />
                 </div>
                 <div>
                     <h2 className="text-xl text-white m-2">Phone</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="phone"value={form.phone} onChange={onChangeHandeler} />
                 </div>
                 <div>
                     <h2 className="text-xl text-white m-2">Address</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="address"value={form.address} onChange={onChangeHandeler} />
                 </div>
                 <div>
                     <h2 className="text-xl text-white m-2">Postal Code</h2>
-                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="" />
+                    <input className="border-2 rounded-md border-indigo-300 w-full bg-zinc-900 h-10 outline-none text-white p-2 text-sm" type="text" name="postalCode"value={form.postalCode} onChange={onChangeHandeler}/>
                 </div>
             </div>
             
--- components/templates/AddCustomer.js
+++ components/templates/AddCustomer.js
@@ -20,9 +20,9 @@ const AddCustomer = () => {
 
     return (
         <>
-            <div className="flex flex-col items-center justify-between rounded-md border-2 w-80 pt-4 m-auto">
+            <div className="flex flex-col items-center justify-between rounded-md border-2 w-2/4 pt-4 m-auto">
                 <h1 className="text-2xl text-white m-5">Create a New Customer</h1>
-                <Form form={form} setForm={setForm}></Form>
+                <Form form={form} setForm={setForm}/>
                 <div className="w-full flex items-center justify-between">
                     <button className="p-2 border rounded-md border-red-400 text-white m-4" onChange={cancelHandeler}>Cancel</button>
                     <button className="p-2 border rounded-md border-green-400 text-white m-4" onChange={SaveHandeler}>Save</button>
