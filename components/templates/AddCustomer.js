da4005ef9574a0ec017a9465867d3636e81535c6
NEX-DoT <nex.dot2@gmail.com>
2023-10-12 18:32:38 +0330

    btn action created { POST and Canceling }

--- components/templates/AddCustomer.js
+++ components/templates/AddCustomer.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 import Form from "../models/Form";
 const AddCustomer = () => {
+    const router = useRouter();
     const [form , setForm] = useState({
         name: "",
         lastName: "",
@@ -11,21 +13,37 @@ const AddCustomer = () => {
         date:"",
         products:[],
     })
-    const cancelHandeler = ()=>{
-    
-    } 
-    const SaveHandeler = ()=>{
-    
-    }
+ const cancelHandeler = ()=>{
+    setForm({
+        name: "",
+        lastName: "",
+        email:"",
+        phone:"",
+        address:"",
+        postalCode:"",
+        date:"",
+        products:[],
+    })
+    router.push('/')
+ }
+ const SaveHandeler = async () => {
+    const res = await fetch('/api/customer/' , {
+        method: 'POST',
+        body: JSON.stringify({data :form}),
+        headers : { 'Content-Type':'application/json'}
+    })
+    const DATA = await res.json();
+    console.log(DATA);
 
+ }
     return (
         <>
             <div className="flex flex-col items-center justify-between rounded-md border-2 w-2/4 pt-4 m-auto">
                 <h1 className="text-2xl text-white m-5">Create a New Customer</h1>
                 <Form form={form} setForm={setForm}/>
                 <div className="w-full flex items-center justify-between">
-                    <button className="p-2 border rounded-md border-red-400 text-white m-4" onChange={cancelHandeler}>Cancel</button>
-                    <button className="p-2 border rounded-md border-green-400 text-white m-4" onChange={SaveHandeler}>Save</button>
+                    <button className="p-2 border rounded-md border-red-400 text-white m-4" onClick={cancelHandeler}>Cancel</button>
+                    <button className="p-2 border rounded-md border-green-400 text-white m-4" onClick={SaveHandeler}>Save</button>
                 </div>
             </div>
         </>
