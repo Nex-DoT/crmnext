0e158bdcecf6a798a5a734aff932e4b64fb710f2
NEX-DoT <nex.dot2@gmail.com>
2023-10-16 11:27:14 +0330

    some update

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
                <h2 className="text-xl">Botostart CRM</h2>
                <Link className="bg-green-500 p-2 rounded-md" href="/add-customer">Add Customer</Link>
            </header>
-           <main className=" bg-zinc-950">{children}</main>
+           <main className=" bg-zinc-950 p-4">{children}</main>
            <footer className="flex  justify-center p-2 bg-zinc-900 text-white">
                Next.js course | CRM Project &copy;  
            </footer>            
--- pages/add-customer/index.js
+++ pages/add-customer/index.js
@@ -2,7 +2,7 @@ import AddCustomer from "@/components/templates/AddCustomer";
 
 const index = () => {
     return (
-        <div className=" w-full h-full flex  pt-10 pb-10">
+        <div className=" w-full h-full flex pt-10 pb-10">
             <AddCustomer/>
         </div>
     );
